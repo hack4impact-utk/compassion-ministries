39534b4deaee527aa45909d7863c318a1811ac89
djwoun <65102751+djwoun@users.noreply.github.com>
2023-10-30 22:37:22 -0400

    update organization

--- src/server/actions/Organizations.ts
+++ src/server/actions/Organizations.ts
@@ -1,15 +1,17 @@
 import dbConnect from '@/utils/db-connect';
 import OrganizationSchema from '../models/Organization';
 
-export async function createOrganization() {
+export async function createOrganization(): Promise<string> {
 //name: string,
-//softDelete: boolean,
-  await dbConnect();
-
-  const organization = await OrganizationSchema.create({
-    //name: name,
-    //softDelete: softDelete,
-  });
-
-  return organization;
+//softDelete: boolean
+  try {
+    await dbConnect();
+    const organization = await OrganizationSchema.create({
+      //name: name,
+      //softDelete: softDelete,
+    });
+    return organization._id.toString(); // Return the _id as a string
+  } catch (error) {
+    throw new Error('Failed to create organization');
+  }
 }
