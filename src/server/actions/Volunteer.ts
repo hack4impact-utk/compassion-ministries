fdb4addb2798380772a7ea93eda71c9c5e99ec43
ethanhmaness <ethanhmaness@gmail.com>
2023-11-06 16:53:23 -0500

    added server func comment

--- src/server/actions/Volunteer.ts
+++ src/server/actions/Volunteer.ts
@@ -2,6 +2,10 @@ import dbConnect from '@/utils/db-connect';
 import VolunteerSchema from '@/server/models/Volunteer';
 import { CreateVolunteerRequest } from '@/types/dataModel/volunteer';
 
+/**
+ * Creates a volunteer
+ * @returns The id of the new volunteer
+ */
 export async function createVolunteer(
   request: CreateVolunteerRequest
 ): Promise<string> {
