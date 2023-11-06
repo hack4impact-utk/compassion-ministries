f7b0330bd735adffbeec19ed904569b5c1828fd1
ethanhmaness <ethanhmaness@gmail.com>
2023-11-06 16:45:26 -0500

    Create an API route for deleting a volunteer
    Added API route and server func for deleting volunteer from database by ID. Note that the {strict: false} option is required for it to work on objects that do not have a pre-existing softDelete field set to false.

---
+++ src/app/api/volunteers/[volunteerId]/route.ts
@@ -0,0 +1,25 @@
+import { softDeleteVolunteer } from '@/server/actions/Volunteers';
+import { zObjectId } from '@/types/dataModel/base';
+import { NextRequest, NextResponse } from 'next/server';
+
+// @route DELETE /api/volunteers/[organizationId] - Soft deletes an organization
+export async function DELETE(
+  _request: NextRequest,
+  { params }: { params: { volunteerId: string } }
+) {
+  const validationResult = zObjectId.safeParse(params.volunteerId);
+  if (!validationResult.success) {
+    return NextResponse.json(
+      { message: 'Invalid Volunteer Id' },
+      { status: 400 }
+    );
+  }
+  const res = await softDeleteVolunteer(params.volunteerId);
+  if (!res || res.softDelete) {
+    return NextResponse.json(
+      { message: 'Volunteer not found' },
+      { status: 404 }
+    );
+  }
+  return new NextResponse(undefined, { status: 204 });
+}
---
+++ src/server/actions/Volunteers.ts
@@ -0,0 +1,22 @@
+import dbConnect from '@/utils/db-connect';
+import VolunteerSchema from '@/server/models/Volunteer';
+import { VolunteerEntity } from '@/types/dataModel/volunteer';
+
+/**
+ * Soft delete a volunteer
+ * @param volunteerId The Id of the volunteer to be deleted
+ * @returns The volunteer in the database before soft-deletion, or null
+ */
+export async function softDeleteVolunteer(
+  volunteerId: string
+): Promise<VolunteerEntity | null> {
+  await dbConnect();
+
+  const res: VolunteerEntity | null = await VolunteerSchema.findByIdAndUpdate(
+    volunteerId,
+    { softDelete: true },
+    { strict: false } //Makes sure to add a softDelete field to the volunteer if it does not already have one
+  );
+
+  return res;
+}
