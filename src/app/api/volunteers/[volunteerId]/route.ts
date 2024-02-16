05656fa4d19557b60aaa84723bcb83eb1d1afe33
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-16 14:20:00 -0500

    updated style and comments

--- src/app/api/volunteers/[volunteerId]/route.ts
+++ src/app/api/volunteers/[volunteerId]/route.ts
@@ -12,15 +12,15 @@ export async function DELETE(
 ) {
   try {
     const validationResult = zObjectId.safeParse(params.volunteerId);
-  if (!validationResult.success) {
-    return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
-  }
-  const res = await softDeleteVolunteer(params.volunteerId);
-  if (!res || res.softDelete) {
-    return new CMError(CMErrorType.NoSuchKey, 'Volunteer').toNextResponse();
-  }
+    if (!validationResult.success) {
+      return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
+    }
+    const res = await softDeleteVolunteer(params.volunteerId);
+    if (!res || res.softDelete) {
+      return new CMError(CMErrorType.NoSuchKey, 'Volunteer').toNextResponse();
+    }
 
-  return new NextResponse(undefined, { status: 204 });
+    return new NextResponse(undefined, { status: 204 });
   } catch(error) {
     return CMErrorResponse(error);
   }
@@ -31,31 +31,30 @@ export async function PUT(
   { params }: { params: { volunteerId: string } }
 ) {
   // validate volunteer id
-  const objectIdValidationResult = zObjectId.safeParse(params.volunteerId);
-  if (!objectIdValidationResult.success) {
-    // This was originally a bad body error
-    return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
-  }
-
-  // validate request body is a valid updateVolunteerRequest
-  const body = await request.json();
-  const updateVolunteerRequestValidationResult =
-    zUpdateVolunteerRequest.safeParse(body);
-  if (!updateVolunteerRequestValidationResult.success) {
-    return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
-  }
-
-  // update the volunteer
   try {
+    const objectIdValidationResult = zObjectId.safeParse(params.volunteerId);
+    if (!objectIdValidationResult.success) {
+      // This was originally a bad body error
+      return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
+    }
+
+    // validate request body is a valid updateVolunteerRequest
+    const body = await request.json();
+    const updateVolunteerRequestValidationResult =
+    zUpdateVolunteerRequest.safeParse(body);
+    if (!updateVolunteerRequestValidationResult.success) {
+      return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
+    }
+    
+    // update the volunteer
     await updateVolunteer(
       params.volunteerId,
       updateVolunteerRequestValidationResult.data
     );
+    return new NextResponse(undefined, { status: 204 });
   } catch (error) {
     return CMErrorResponse(error);
   }
-
-  return new NextResponse(undefined, { status: 204 });
 }
 
 // @route GET /api/volunteers/[volunteerId] - get a specific volunteer
