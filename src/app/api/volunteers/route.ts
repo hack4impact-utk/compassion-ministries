3af871700e5edd2c936797505cb3dd3b6619c5de
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-16 14:04:45 -0500

    more error checking updates

--- src/app/api/volunteers/route.ts
+++ src/app/api/volunteers/route.ts
@@ -1,6 +1,7 @@
 import { getAllVolunteers, createVolunteer } from '@/server/actions/Volunteer';
 import { NextRequest, NextResponse } from 'next/server';
 import { zCreateVolunteerRequest } from '@/types/dataModel/volunteer';
+import CMError, { CMErrorResponse, CMErrorType } from '@/utils/cmerror';
 
 // @route GET /api/volunteers - Get all volunteers
 export async function GET() {
@@ -8,10 +9,7 @@ export async function GET() {
     const volunteers = await getAllVolunteers();
     return NextResponse.json(volunteers, { status: 200 });
   } catch (error) {
-    return NextResponse.json(
-      { message: 'Internal server error' },
-      { status: 500 }
-    );
+    return CMErrorResponse(error);
   }
 }
 
@@ -21,24 +19,16 @@ export async function POST(request: NextRequest) {
     const req = await request.json();
     const validationResult = zCreateVolunteerRequest.safeParse(req);
     if (!validationResult.success) {
-      return NextResponse.json(
-        { message: 'Invalid Request Body' },
-        { status: 400 }
-      );
+      // this was originally a bad body error using 400 so I might make new one
+      return new CMError(CMErrorType.BadValue, 'Volunteer').toNextResponse();
     }
 
     const res = await createVolunteer(validationResult.data);
     return NextResponse.json({ _id: res }, { status: 201 });
   } catch (error) {
     if ((error = 11000)) {
-      return NextResponse.json(
-        { message: 'Conflict: Duplicate Entry' },
-        { status: 409 }
-      );
+      return new CMError(CMErrorType.DuplicateKey, 'Volunteer').toNextResponse();
     }
-    return NextResponse.json(
-      { message: 'Internal server error' },
-      { status: 500 }
-    );
+    return CMErrorResponse(error);
   }
 }
