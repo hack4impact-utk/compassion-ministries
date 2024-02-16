1e16a880a3f8842d96ed0355f308c7ed60e0b8dd
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-16 14:35:06 -0500

    optics

--- src/server/actions/Volunteers.ts
+++ src/server/actions/Volunteers.ts
@@ -17,14 +17,14 @@ export async function softDeleteVolunteer(
 ): Promise<VolunteerResponse | null> {
   let res: VolunteerResponse | null = null;
   try {
-  await dbConnect();
-  res = await VolunteerSchema.findByIdAndUpdate(
-    volunteerId,
-    { softDelete: true }
-  );
+    await dbConnect();
+    res = await VolunteerSchema.findByIdAndUpdate(
+      volunteerId,
+      { softDelete: true }
+    );
 
   } catch (error) {
-    throw new CMError(CMErrorType.InternalError, "Server");
+      throw new CMError(CMErrorType.InternalError, "Server");
   }
   if (!res) {
     throw new CMError(CMErrorType.NoSuchKey, "Volunteer");
@@ -47,7 +47,7 @@ export async function getVolunteer(
      .populate('previousOrganization');
   } catch (error) {
       throw new CMError(CMErrorType.InternalError, "Server");
-    }
+  }
   if (!volunteer) {
     throw new CMError(CMErrorType.NoSuchKey, "Volunteer");
   }
