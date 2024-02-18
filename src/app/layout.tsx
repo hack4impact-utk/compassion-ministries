9763a6daf51646707c370bc2d7421cbaaf066998
djwoun <65102751+djwoun@users.noreply.github.com>
2024-02-17 22:22:35 -0500

    comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  //Render HTML body with Providers and Navbar components, including children.
   return (
     <html lang="en">
       <body>
