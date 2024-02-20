073bad128a204269030f3878fdbaf3f924de43d3
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-20 13:25:30 -0500

    testing environment and completed page

--- src/app/organizations/Organization/index.tsx
+++ src/app/organizations/Organization/index.tsx
@@ -17,47 +17,18 @@ export default function OrganizationInfo({
   return (
     <Box>
       <Typography variant="h1">{organization.name}</Typography>
-      <Box sx={{ display: 'flex' }}>
-        <Typography variant="subtitle1" sx={{ fontWeight: 'bold', pr: 1 }}>
-          Address:
-        </Typography>
-        <Typography display="inline">{organization.address}</Typography>
-      </Box>
-      <Box sx={{ display: 'flex' }}>
-        <Typography variant="subtitle1" sx={{ fontWeight: 'bold', pr: 1 }}>
-          Phone number:
-        </Typography>
-        <Typography display="inline">{organization.phoneNumber}</Typography>
-      </Box>
-      <Box sx={{ display: 'flex' }}>
-        <Typography variant="subtitle1" sx={{ fontWeight: 'bold', pr: 1 }}>
-          Email:
-        </Typography>
-        <Typography display="inline">{organization.email}</Typography>
-      </Box>
       <Typography variant="h5">Volunteers</Typography>
       {volunteers.map((volunteer, index) => (
         <Box key={index}>
           <Box sx={{ display: 'flex' }}>
-            <Typography variant="subtitle1" sx={{ fontWeight: 'bold', pr: 1 }}>
-              Name:
-            </Typography>
             <Typography display="inline">
               {volunteer.firstName} {volunteer.lastName}
             </Typography>
           </Box>
           <Box sx={{ display: 'flex' }}>
-            <Typography variant="subtitle1" sx={{ fontWeight: 'bold', pr: 1 }}>
-              Email:
-            </Typography>
             <Typography display="inline">{volunteer.email}</Typography>
           </Box>
-          <Box sx={{ display: 'flex' }}>
-            <Typography variant="subtitle1" sx={{ fontWeight: 'bold', pr: 1 }}>
-              Phone number:
-            </Typography>
-            <Typography display="inline">{volunteer.phoneNumber}</Typography>
-          </Box>
+          <br />
         </Box>
       ))}
     </Box>
--- src/app/organizations/[organizationId]/page.tsx
+++ src/app/organizations/[organizationId]/page.tsx
@@ -1,3 +1,100 @@
-export default function Page( { params }: { params: { organizationId: string } }) {
-    return <h1> Organizations Page {params.organizationId} </h1>;
+import { VolunteerResponse } from "@/types/dataModel/volunteer";
+import { OrganizationResponse } from "@/types/dataModel/organization";
+import OrganizationInfo from "../Organization";
+
+const sampleOrganization: OrganizationResponse = {
+    name: 'Yep, Another Organization',
+    softDelete: false,
+    _id: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+};
+
+const sampleVolunteers: VolunteerResponse[] = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      phoneNumber: '123-456-7890',
+      address: '123 Main St, Anytown, USA',
+      softDelete: false,
+      previousOrganization: {
+        name: 'Previous Organization',
+        softDelete: false,
+        _id: 'organization_id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      _id: 'volunteer_id_1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      roleVerifications: [
+        {
+          role: 'Medical',
+          verifier: 'Verifier 1',
+          lastUpdated: new Date(),
+        },
+      ],
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      email: 'jane.smith@example.com',
+      phoneNumber: '987-654-3210',
+      address: '456 Oak St, Othertown, USA',
+      softDelete: false,
+      previousOrganization: {
+        name: 'Another Organization',
+        softDelete: false,
+        _id: 'another_organization_id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      _id: 'volunteer_id_2',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      roleVerifications: [
+        {
+          role: 'Medical',
+          verifier: 'Verifier 2',
+          lastUpdated: new Date(),
+        },
+      ],
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      email: 'alice.johnson@example.com',
+      phoneNumber: '555-123-4567',
+      address: '789 Elm St, Newcity, USA',
+      softDelete: false,
+      previousOrganization: {
+        name: 'Yet Another Organization',
+        softDelete: false,
+        _id: 'yet_another_organization_id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      _id: 'volunteer_id_3',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      roleVerifications: [
+        {
+          role: 'Dental',
+          verifier: 'Verifier 3',
+          lastUpdated: new Date(),
+        },
+      ],
+    },
+];
+  
+export default function Page({
+  params,
+}: {
+  params: { organizationId: string };
+}) {
+  return <h1> Organizations Page {params.organizationId} 
+  <OrganizationInfo organization={sampleOrganization} 
+  volunteers={sampleVolunteers} />
+  </h1>;
 }
