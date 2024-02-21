50c2ed974ca80404b108540c009738d637e15b45
ethanhmaness <ethanhmaness@gmail.com>
2024-02-21 11:14:17 -0500

    comments

--- src/components/organizations/OrganizationList/index.tsx
+++ src/components/organizations/OrganizationList/index.tsx
@@ -1,6 +1,8 @@
 import { OrganizationResponse } from '@/types/dataModel/organization';
 import { List, ListItem, ListItemText } from '@mui/material';
 
+// Returns a List containing a ListItem containing a text component with the name of the
+// Organization for each provided Organization
 export default function OrganizationList({
   organizationResponses,
 }: {
