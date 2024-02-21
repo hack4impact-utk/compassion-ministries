5f7d2306ea3b71918dbcb4445f5e94c50cb63737
ethanhmaness <ethanhmaness@gmail.com>
2024-02-21 11:39:20 -0500

    removed extraneous syntax

--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -6,8 +6,8 @@ import { ReactNode } from 'react';
 
 // SVG icons for each event role
 export const roleIcons: { [id: string]: ReactNode } = {
-  ['Food']: <LocalDiningIcon sx={{ color: '#F5A81C' }} />,
-  ['Medical']: <LocalHospitalIcon sx={{ color: 'red' }} />,
-  ['Dental']: <MasksIcon sx={{ color: 'lightblue' }} />,
-  ['Save the Babies']: <ChildFriendlyIcon sx={{ color: 'lightgray' }} />,
+  Food: <LocalDiningIcon sx={{ color: '#F5A81C' }} />,
+  Medical: <LocalHospitalIcon sx={{ color: 'red' }} />,
+  Dental: <MasksIcon sx={{ color: 'lightblue' }} />,
+  'Save the Babies': <ChildFriendlyIcon sx={{ color: 'lightgray' }} />,
 };
