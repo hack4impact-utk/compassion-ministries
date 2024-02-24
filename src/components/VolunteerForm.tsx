22408922a310cf3169c25955abaad7bf4c3d2a51
djwoun <65102751+djwoun@users.noreply.github.com>
2024-02-24 18:07:48 -0500

    using useffect and volunteerform

--- src/components/VolunteerForm.tsx
+++ src/components/VolunteerForm.tsx
@@ -1,72 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, TextField } from '@mui/material';
 import { UpsertVolunteerFormData } from '@/types/forms/volunteer';
 import { VolunteerResponse } from '@/types/dataModel/volunteer';
 
-// UpsertVolunteerFormData for change, Receive the current volunteer from Volunteer Response
 interface VolunteerFormProps {
   onChange: (volunteer: UpsertVolunteerFormData) => void;
   currentVolunteer?: VolunteerResponse;
+  volunteerData: UpsertVolunteerFormData;
 }
 
-// Volunteer form to update name, email, and number
-function VolunteerForm({ onChange, currentVolunteer }: VolunteerFormProps) {
+function VolunteerForm({
+  onChange,
+  currentVolunteer,
+  volunteerData,
+}: VolunteerFormProps) {
+  const [formData, setVolunteerData] =
+    useState<UpsertVolunteerFormData>(volunteerData);
+
+  useEffect(() => {
+    if (currentVolunteer) {
+      // Update form data with current volunteer data
+      setVolunteerData({
+        firstName: currentVolunteer.firstName || '',
+        lastName: currentVolunteer.lastName || '',
+        email: currentVolunteer.email || '',
+        phoneNumber: currentVolunteer.phoneNumber || '',
+        address: currentVolunteer.address || '',
+      });
+    }
+  }, [currentVolunteer]);
+
   const handleChange = (
     field: keyof UpsertVolunteerFormData,
     value: string
   ) => {
-    if (currentVolunteer) {
-      onChange({
-        ...currentVolunteer,
-        [field]: value,
-      });
-    }
+    // Update form data
+    setVolunteerData({
+      ...formData,
+      [field]: value,
+    });
+    // Notify parent component about the change
+    onChange({
+      ...formData,
+      [field]: value,
+    });
   };
 
   const isValidEmail = (email: string) => {
-    // Regular expression for validating email addresses
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const email = e.target.value;
-    if (email === '' || isValidEmail(email)) {
-      handleChange('email', email);
-    }
+
+    handleChange('email', email);
   };
 
-  const emailError =
-    currentVolunteer?.email && !isValidEmail(currentVolunteer.email);
+  const emailError = formData.email && !isValidEmail(formData.email);
 
   return (
-    <Box>
+    <Box pt={2}>
       <TextField
         label="First Name"
-        value={currentVolunteer?.firstName || ''}
+        value={formData.firstName}
         onChange={(e) => handleChange('firstName', e.target.value)}
+        InputLabelProps={{ shrink: !!formData.firstName }}
       />
       <TextField
         label="Last Name"
-        value={currentVolunteer?.lastName || ''}
+        value={formData.lastName}
         onChange={(e) => handleChange('lastName', e.target.value)}
+        InputLabelProps={{ shrink: !!formData.lastName }}
       />
       <TextField
         label="Email"
         type="email"
-        value={currentVolunteer?.email || ''}
+        value={formData.email}
         onChange={handleEmailChange}
         error={emailError ? true : undefined}
+        InputLabelProps={{ shrink: !!formData.email }}
       />
       <TextField
         label="Phone Number"
-        value={currentVolunteer?.phoneNumber || ''}
+        value={formData.phoneNumber}
         onChange={(e) => handleChange('phoneNumber', e.target.value)}
+        InputLabelProps={{ shrink: !!formData.phoneNumber }}
       />
       <TextField
         label="Address"
-        value={currentVolunteer?.address || ''}
+        value={formData.address}
         onChange={(e) => handleChange('address', e.target.value)}
+        InputLabelProps={{ shrink: !!formData.address }}
       />
     </Box>
   );
