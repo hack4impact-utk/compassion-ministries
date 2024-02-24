20d844599cefb2ece4636bcdc46dc0b83cc8d954
djwoun <65102751+djwoun@users.noreply.github.com>
2024-02-24 18:59:20 -0500

    Update VolunteerForm.tsx

--- src/components/VolunteerForm.tsx
+++ src/components/VolunteerForm.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 import { Box, TextField } from '@mui/material';
 import { UpsertVolunteerFormData } from '@/types/forms/volunteer';
 import { VolunteerResponse } from '@/types/dataModel/volunteer';
-
+import { useEffect } from 'react';
 interface VolunteerFormProps {
   onChange: (volunteer: UpsertVolunteerFormData) => void;
   currentVolunteer?: VolunteerResponse;
@@ -14,13 +14,10 @@ function VolunteerForm({
   currentVolunteer,
   volunteerData,
 }: VolunteerFormProps) {
-  const [formData, setVolunteerData] =
-    useState<UpsertVolunteerFormData>(volunteerData);
-
   useEffect(() => {
     if (currentVolunteer) {
       // Update form data with current volunteer data
-      setVolunteerData({
+      onChange({
         firstName: currentVolunteer.firstName || '',
         lastName: currentVolunteer.lastName || '',
         email: currentVolunteer.email || '',
@@ -30,68 +27,46 @@ function VolunteerForm({
     }
   }, [currentVolunteer]);
 
-  const handleChange = (
-    field: keyof UpsertVolunteerFormData,
-    value: string
-  ) => {
-    // Update form data
-    setVolunteerData({
-      ...formData,
-      [field]: value,
-    });
-    // Notify parent component about the change
-    onChange({
-      ...formData,
-      [field]: value,
-    });
-  };
-
-  const isValidEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const email = e.target.value;
-
-    handleChange('email', email);
-  };
-
-  const emailError = formData.email && !isValidEmail(formData.email);
-
   return (
     <Box pt={2}>
       <TextField
         label="First Name"
-        value={formData.firstName}
-        onChange={(e) => handleChange('firstName', e.target.value)}
-        InputLabelProps={{ shrink: !!formData.firstName }}
+        value={volunteerData.firstName}
+        onChange={(e) =>
+          onChange({ ...volunteerData, firstName: e.target.value })
+        }
+        InputLabelProps={{ shrink: !!volunteerData.firstName }}
       />
       <TextField
         label="Last Name"
-        value={formData.lastName}
-        onChange={(e) => handleChange('lastName', e.target.value)}
-        InputLabelProps={{ shrink: !!formData.lastName }}
+        value={volunteerData.lastName}
+        onChange={(e) =>
+          onChange({ ...volunteerData, lastName: e.target.value })
+        }
+        InputLabelProps={{ shrink: !!volunteerData.lastName }}
       />
       <TextField
         label="Email"
         type="email"
-        value={formData.email}
-        onChange={handleEmailChange}
-        error={emailError ? true : undefined}
-        InputLabelProps={{ shrink: !!formData.email }}
+        value={volunteerData.email}
+        onChange={(e) => onChange({ ...volunteerData, email: e.target.value })}
+        InputLabelProps={{ shrink: !!volunteerData.email }}
       />
       <TextField
         label="Phone Number"
-        value={formData.phoneNumber}
-        onChange={(e) => handleChange('phoneNumber', e.target.value)}
-        InputLabelProps={{ shrink: !!formData.phoneNumber }}
+        value={volunteerData.phoneNumber}
+        onChange={(e) =>
+          onChange({ ...volunteerData, phoneNumber: e.target.value })
+        }
+        InputLabelProps={{ shrink: !!volunteerData.phoneNumber }}
       />
       <TextField
         label="Address"
-        value={formData.address}
-        onChange={(e) => handleChange('address', e.target.value)}
-        InputLabelProps={{ shrink: !!formData.address }}
+        value={volunteerData.address}
+        onChange={(e) =>
+          onChange({ ...volunteerData, address: e.target.value })
+        }
+        InputLabelProps={{ shrink: !!volunteerData.address }}
       />
     </Box>
   );
