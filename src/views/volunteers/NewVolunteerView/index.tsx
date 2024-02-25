9a3ae8f48f3a6647bccbfe08f01182f973d040ac
Zavier Miller <zavierjmiller@gmail.com>
2024-02-24 22:44:46 -0500

    hook up to backend

--- src/views/volunteers/NewVolunteerView/index.tsx
+++ src/views/volunteers/NewVolunteerView/index.tsx
@@ -5,19 +5,34 @@ import { UpsertVolunteerFormData } from '@/types/forms/volunteer';
 import { Button, Box } from '@mui/material';
 
 export default function NewVolunteerView() {
-  const [volunteers, setVolunteerData] = useState<UpsertVolunteerFormData>(
+  const [volunteer, setVolunteerData] = useState<UpsertVolunteerFormData>(
     {} as UpsertVolunteerFormData
   );
 
-  const submitData = () => {
-    // Placeholder submit function
-    // TODO: replace with actual implementation
-    console.log('Volunteer data submitted:', volunteers);
+  const submitData = async () => {
+    const res = await fetch('/api/volunteers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(volunteer),
+    });
+
+    const data = await res.json();
+
+    if (res.status !== 201) {
+      console.error(
+        'failed to create volunteer. volunteer: ',
+        volunteer,
+        'response: ',
+        data
+      );
+    }
   };
 
   return (
     <Box>
-      <VolunteerForm onChange={setVolunteerData} volunteerData={volunteers} />
+      <VolunteerForm onChange={setVolunteerData} volunteerData={volunteer} />
       <Button variant="contained" fullWidth type="submit" onClick={submitData}>
         Submit
       </Button>
