e80d3ff257d11dfd96064a02f4167131084052a6
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-24 23:32:32 -0500

    idk why it does this

--- src/app/organization/[organizationId]/page.tsx
+++ src/app/organization/[organizationId]/page.tsx
@@ -1,11 +1,11 @@
+import { OrganizationView } from '@/app/views/OrganizationView';
 import { getOrganization } from '@/server/actions/Organization';
-import OrganizationView from '../views/Organization/OrganizationView';
 
 export default async function Page({
   params,
 }: {
   params: { organization: string };
 }) {
-  const orgs = await getOrganization(params.organization);
-  return <OrganizationView organization={orgs} />;
+  const org = await getOrganization(params.organization);
+  return org ? <OrganizationView organization={org} /> : null;
 }
--- src/app/views/OrganizationView/index.tsx
+++ src/app/views/OrganizationView/index.tsx
@@ -1,17 +1,17 @@
-import { OrganizationResponse } from "@/types/dataModel/organization";
-
+'use client';
+import { OrganizationResponse } from '@/types/dataModel/organization';
+import React from 'react';
 
 interface OrganizationViewProps {
-    organization: OrganizationResponse;
+  organization: OrganizationResponse;
 }
 
-export function OrganizationView({
-    organization, 
-}: OrganizationViewProps) {
-    return (
-        <div>
-            <h1>Organization</h1>
-            <p>Name: {organization.name}</p>
-        </div>
-    );
+export function OrganizationView({ organization }: OrganizationViewProps) {
+  return (
+    <div>
+      <h1>Organization</h1>
+      <p>Name: {organization.name}</p>
+      <button>Edit</button>
+    </div>
+  );
 }
