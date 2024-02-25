32721f6a5ad3b17c4250941001ae908c525c2f8f
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-24 21:21:13 -0500

    update test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { getEventsBetweenDates } from "@/server/actions/Event";
-
 export default function Home() {
-  const today = new Date();
-  const todaymin30 = new Date();
-  todaymin30.setDate(todaymin30.getDate() - 10000);
-  getEventsBetweenDates(todaymin30, today);
   return <h1>Home page</h1>;
 }
