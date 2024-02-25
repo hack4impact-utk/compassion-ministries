2a901ec1198ac3444442b22550ae59b389d9cb8c
Zavier Miller <zavierjmiller@gmail.com>
2024-02-25 15:59:32 -0500

    remove changes to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-'use client';
-import useRoleConfirmation from '@/hooks/useRoleConfirmation';
-import { Button } from '@mui/material';
-
 export default function Home() {
-  const confirm = useRoleConfirmation();
-
-  const handleConfirm = async () => {
-    const verifier = await confirm('admin');
-    console.log('what???', verifier);
-  };
-
-  return (
-    <>
-      <h1>Home page</h1>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => handleConfirm()}
-      >
-        Test
-      </Button>
-    </>
-  );
+  return <h1>Home page</h1>;
 }
