1c092bee2103c2c8fc80958178e0d708516360f8
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-26 13:58:12 -0500

    finished, need to test

--- src/server/actions/EventVolunteer.ts
+++ src/server/actions/EventVolunteer.ts
@@ -1,14 +1,24 @@
+import { EventVolunteerResponse } from '@/types/dataModel/eventVolunteer';
 import EventVolunteerShema from '../models/EventVolunteer';
-import EventVolunteerReponse from '../models/EventVolunteer';
-import dbConnect from '../utils/db-connect';
+import dbConnect from '@/utils/db-connect';
+import CMError, { CMErrorType } from '@/utils/cmerror';
 
+/**
+ * Get all volunteers for a specific event
+ * @param eventId // Id of the event
+ * @returns // All volunteers for the event
+ */
 export async function getEventVolunteersByOrganization(
   organizationId: string
-): Promise<EventVolunteerReponse[]> {
-  await dbConnect();
-  // call the EventVolunteerSchema.find and search by the organization field on EventVoluneer. Return this list
-  const events = await EventVolunteerShema.find({
-    organization: organizationId,
-  });
-  return events;
+): Promise<EventVolunteerResponse[]> {
+  try {
+    await dbConnect();
+    const events: EventVolunteerResponse[] = await EventVolunteerShema.find({
+      organization: organizationId,
+    });
+
+    return events;
+  } catch (error) {
+    throw new CMError(CMErrorType.InternalError);
+  }
 }
