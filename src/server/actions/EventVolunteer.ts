cc8c544cac1566592627bebed32e62e9c06751c6
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-02-26 15:35:36 -0500

    event to org

--- src/server/actions/EventVolunteer.ts
+++ src/server/actions/EventVolunteer.ts
@@ -5,7 +5,7 @@ import CMError, { CMErrorType } from '@/utils/cmerror';
 
 /**
  * Get all volunteers for a specific organization
- * @param eventId // Id of the event
+ * @param organizationId // Id of the organization
  * @returns // All volunteers for the organization
  */
 export async function getEventVolunteersByOrganization(
