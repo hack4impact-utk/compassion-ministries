ba9841e5f2089524a018e8c4cdf0dfa8fedb18b1
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-03-01 13:00:11 -0500

    context swtich

--- src/server/actions/EventVolunteer.ts
+++ src/server/actions/EventVolunteer.ts
@@ -1,26 +1,26 @@
-import { EventVolunteerResponse } from '@/types/dataModel/eventVolunteer';
-import EventVolunteerShema from '../models/EventVolunteer';
 import dbConnect from '@/utils/db-connect';
 import CMError, { CMErrorType } from '@/utils/cmerror';
+import { VolunteerEntity } from '@/types/dataModel/volunteer';
+import EventVolunteerShema from '../models/EventVolunteer';
 
 /**
  * Get all volunteers for a specific organization
  * @param organizationId // Id of the organization
- * @returns // All volunteers for the organization
- */
-export async function getEventVolunteersByOrganization(
+ * @returns // All volunteers for the organization */
+export async function getVolunteersByOrganization(
   organizationId: string
-): Promise<EventVolunteerResponse[]> {
+): Promise<VolunteerEntity[]> {
+  // Investigate if mongoose will only return unique volunteers
   try {
     await dbConnect();
-    const volunteers: EventVolunteerResponse[] = await EventVolunteerShema.find(
-      {
-        organization: organizationId,
-      }
-    );
-    // console.log(volunteers);
+    const volunteers: VolunteerEntity[] = await EventVolunteerShema.find({
+      organization: organizationId,
+    }).distinct('volunteer');
 
-    return volunteers;
+    // map the result into an array of VolunteerEntity
+    const volunteerEntities: VolunteerEntity[] = volunteers.map(
+      (volunteer) => { volunteer: volunteerEntity.volunteer }
+    );
   } catch (error) {
     throw new CMError(CMErrorType.InternalError);
   }
