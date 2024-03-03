38b88e0b316cf6a2a121f439f03a1e1f3b2f7218
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-03-03 13:42:02 -0500

    break

--- src/app/views/OrganizationView/index.tsx
+++ src/app/views/OrganizationView/index.tsx
@@ -2,18 +2,26 @@
 import { OrganizationResponse } from '@/types/dataModel/organization';
 import React from 'react';
 import OrganizationInfo from '@/components/Organization';
+import { getVolunteersByOrganization } from '@/server/actions/Volunteer';
+import { VolunteerResponse } from '@/types/dataModel/volunteer';
+import { Button } from '@mui/material';
 
 interface OrganizationViewProps {
   organization: OrganizationResponse;
 }
 
-export function OrganizationView({ organization }: OrganizationViewProps) {
+export async function OrganizationView({
+  organization,
+}: OrganizationViewProps) {
   // TODO: get volunteers from organization and pass them to the OrganizationList
+  const volunteers: VolunteerResponse[] = await getVolunteersByOrganization(
+    organization._id
+  );
   return (
     <div>
       <h1>Organization</h1>
-      <OrganizationInfo organization={[organization]} />
-      <button>Edit</button>
+      <OrganizationInfo organization={organization} volunteers={volunteers} />
+      <Button variant="contained">edit</Button>
     </div>
   );
 }
