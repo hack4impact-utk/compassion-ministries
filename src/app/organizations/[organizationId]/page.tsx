40ad59003520cd686e11917557fd179282bacadb
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-03-04 11:49:25 -0500

    review implementation #1 and testing

--- src/app/organizations/[organizationId]/page.tsx
+++ src/app/organizations/[organizationId]/page.tsx
@@ -1,7 +1,13 @@
-export default function Page({
+import { OrganizationView } from '@/app/views/OrganizationView';
+import { getOrganization } from '@/server/actions/Organization';
+import { getVolunteersByOrganization } from '@/server/actions/Volunteer';
+
+export default async function Page({
   params,
 }: {
   params: { organizationId: string };
 }) {
-  return <h1> Organizations Page {params.organizationId}</h1>;
+  const org = await getOrganization(params.organizationId);
+  const vols = await getVolunteersByOrganization(params.organizationId);
+  return <OrganizationView organization={org} volunteers={vols} />;
 }
--- src/app/views/OrganizationView/index.tsx
+++ src/app/views/OrganizationView/index.tsx
@@ -1,23 +1,19 @@
+'use client';
 import { OrganizationResponse } from '@/types/dataModel/organization';
 import React from 'react';
 import OrganizationInfo from '@/components/Organization';
-import { getVolunteersByOrganization } from '@/server/actions/Volunteer';
 import { VolunteerResponse } from '@/types/dataModel/volunteer';
 import { Button } from '@mui/material';
 
 interface OrganizationViewProps {
   organization: OrganizationResponse;
+  volunteers: VolunteerResponse[];
 }
 
-export async function OrganizationView({
+export function OrganizationView({
   organization,
+  volunteers,
 }: OrganizationViewProps) {
-  const organizationId = organization._id.toString();
-
-  // Fetch volunteers for the organization
-  const volunteers: VolunteerResponse[] =
-    await getVolunteersByOrganization(organizationId);
-
   return (
     <div>
       {/* Display organization information */}
