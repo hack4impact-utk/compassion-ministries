7f153cd033d364e591143f37e4f3bea798b6764b
JihunKimCode <jiunkim8@gmail.com>
2024-03-12 14:18:20 -0500

    Update EventVolunteerList

--- src/components/EventVolunteerList/index.tsx
+++ src/components/EventVolunteerList/index.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import { getRoleIcons } from '@/utils/role';
 import { EventVolunteerResponse } from '@/types/dataModel/eventVolunteer';
 import Box from '@mui/material/Box';
-import { ForkLeft } from '@mui/icons-material';
+import IconList from '../IconList';
 
 // Prop Array with Objects
 interface EventVolunteerListProps {
@@ -20,17 +17,23 @@ export default function EventVolunteerList({
   return (
     <List>
       {volunteerResponses.map((eventVolunteer, index) => (
-        <ListItem key={index}>
+        <ListItem
+          key={index}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+          }}
+        >
           {/* Show Volunteer information */}
           <ListItemText
             primary={`${eventVolunteer.volunteer.firstName} ${eventVolunteer.volunteer.lastName}`}
-            secondary={`With ${eventVolunteer.volunteer.previousOrganization}`}
+            secondary={`With ${eventVolunteer.organization}`}
           />
           {/* Display Icons of the Volunteer's Role */}
-          <Box sx={{ display: 'flex' }} pt={1} key={index}>
-            {/* Render valid React elements here */}
-            {getRoleIcons([eventVolunteer.role])}
-            {eventVolunteer.role}
+          <Box sx={{ display: 'flex' }}>
+            <IconList roles={[eventVolunteer.role]} />
+            <span>{eventVolunteer.role}</span>
           </Box>
         </ListItem>
       ))}
