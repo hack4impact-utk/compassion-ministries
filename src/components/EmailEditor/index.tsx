00f9201684164d5092792d901470393605c13460
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-03-22 14:30:55 -0400

    added TextField subject

---
+++ src/components/EmailEditor/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { EmailFormData } from '@/types/forms/email';
+import ReactQuill from 'react-quill';
+import { TextField } from '@mui/material';
+
+interface EmailEditorProps {
+  onChange: (formData: EmailFormData) => void;
+  formData: EmailFormData;
+}
+
+export default function EmailEditor({
+  onChange,
+  formData,
+}: EmailEditorProps): React.ReactElement {
+  // textField for the suject
+  return (
+    <div>
+      <TextField
+        label="Subject"
+        value={formData.subject}
+        onChange={(e) => onChange({ ...formData, subject: e.target.value })}
+        fullWidth
+      />
+
+      <ReactQuill
+        value={formData.emailbody}
+        onChange={(value) => onChange({ ...formData, emailbody: value })}
+      />
+    </div>
+  );
+}
