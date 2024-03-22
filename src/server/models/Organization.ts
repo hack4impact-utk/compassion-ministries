79e6839cc4391ba604e7ee12db6f82493e8a9190
Zavier Miller <zavierjmiller@gmail.com>
2024-03-21 20:43:39 -0400

    remove some serialization from orgs view

--- src/server/models/Organization.ts
+++ src/server/models/Organization.ts
@@ -20,6 +20,24 @@ const OrganizationSchema = new Schema(
   }
 );
 
+OrganizationSchema.post('find', function (docs: OrganizationEntity[]) {
+  docs.forEach((doc) => {
+    doc._id = doc._id.toString();
+  });
+});
+
+OrganizationSchema.post('findOne', function (doc: OrganizationEntity) {
+  if (doc) {
+    doc._id = doc._id.toString();
+  }
+});
+
+OrganizationSchema.post(/findById/, function (doc: OrganizationEntity) {
+  if (doc) {
+    doc._id = doc._id.toString();
+  }
+});
+
 export interface OrganizationDocument
   extends Omit<OrganizationEntity, '_id'>,
     Document {}
--- src/types/dataModel/base.ts
+++ src/types/dataModel/base.ts
@@ -1,12 +1,12 @@
-import { ObjectId } from 'bson';
 import { z } from 'zod';
 
-export const zObjectId = z.string().refine((val) => ObjectId.isValid(val));
+// TODO: implement our own validation for this :(
+export const zObjectId = z.string();
 
 const zBase = z.object({
   _id: zObjectId,
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
 });
 
 export interface Base extends z.infer<typeof zBase> {}
