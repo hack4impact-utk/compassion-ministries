3b35193c9075fa10f5c625f5e99850007a04fa19
Andrew Rutter <andlrutt@vols.utk.edu>
2024-03-22 23:44:35 -0400

    events page changes

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,10 +1,24 @@
-import { rrulestr } from "rrule";
+import { EventResponse } from '@/types/dataModel/event';
+import { rrulestr } from 'rrule';
 
 export function datesBetweenFromRrule(
-    rrule: string,
-    startDate: Date,
-    endDate: Date    
+  rrule: string,
+  startDate: Date,
+  endDate: Date
 ): Date[] {
-    const rrule1 = rrulestr(rrule);
-    return rrule1.between(startDate, endDate);   
+  const rrule1 = rrulestr(rrule);
+  return rrule1.between(startDate, endDate);
+}
+
+export function getEventDisplayDate(event: EventResponse) {
+  return `${event.date?.toDateString()}, ${event.startAt.toLocaleTimeString(
+    [],
+    {
+      hour: 'numeric',
+      minute: '2-digit',
+    }
+  )} - ${event.endAt.toLocaleTimeString([], {
+    hour: 'numeric',
+    minute: '2-digit',
+  })}`;
 }
