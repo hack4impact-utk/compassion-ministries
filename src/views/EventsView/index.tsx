676faa65b15252da61a9e2b95373de0efa124f3b
ethanhmaness <ethanhmaness@gmail.com>
2024-03-24 16:00:33 -0400

    first ver

--- src/views/EventsView/index.tsx
+++ src/views/EventsView/index.tsx
@@ -1,15 +1,27 @@
+'use client';
 import EventList from '@/components/EventList';
 import { EventResponse } from '@/types/dataModel/event';
 import { Box, Button, Typography } from '@mui/material';
 import Link from 'next/link';
+import SearchField from '@/components/SearchField';
+import useSearch from '@/hooks/useSearch';
 
 interface EventsViewProps {
   events: EventResponse[];
 }
 
 export default function EventsView({ events }: EventsViewProps) {
+  const search = useSearch();
+
+  if (search.length > 0) {
+    events = events.filter((event) =>
+      `${event.name}`.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
   return (
     <Box>
+      <SearchField />
       <Typography variant="h3" sx={{ mb: 2 }}>
         Events
       </Typography>
--- src/views/OrganizationsView/index.tsx
+++ src/views/OrganizationsView/index.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import OrganizationList from '@/components/organizations/OrganizationList';
 import Link from 'next/link';
 import { Button, Typography } from '@mui/material';
+import SearchField from '@/components/SearchField';
+import useSearch from '@/hooks/useSearch';
 
 interface OrganizationsViewProps {
   organizations: OrganizationResponse[];
@@ -11,8 +13,17 @@ interface OrganizationsViewProps {
 
 // display the OrganizationList component and an edit buttom
 export function OrganizationsView({ organizations }: OrganizationsViewProps) {
+  const search = useSearch();
+
+  if (search.length > 0) {
+    organizations = organizations.filter((organization) =>
+      `${organization.name}`.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
   return (
     <div>
+      <SearchField />
       <Typography variant="h3" sx={{ mb: 2 }}>
         Organizations
       </Typography>
