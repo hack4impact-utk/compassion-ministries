aac3e1bb3327baa06d76cdc1c663452bdb1b9c99
djwoun <65102751+djwoun@users.noreply.github.com>
2024-03-30 05:13:12 -0400

    create zod type

---
+++ src/types/dataModel/BackgroundCheckWebhookPayload.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const zBackgroundCheckWebhookPayload = z.object({
+  data: z.object({
+    employee_email: z.string(),
+    overall_status: z.string(),
+  }),
+  event_type: z.string(),
+});
+
+export interface BackgroundCheckWebhookPayload
+  extends z.infer<typeof zBackgroundCheckWebhookPayload> {}
+
+export default zBackgroundCheckWebhookPayload;
