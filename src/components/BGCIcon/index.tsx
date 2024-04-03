b910a4aa3eb37c248bc2d11b7e9dc948c21cd2df
Zavier Miller <zavierjmiller@gmail.com>
2024-04-03 18:50:40 -0400

    pr changes

--- src/components/BGCIcon/index.tsx
+++ src/components/BGCIcon/index.tsx
@@ -1,47 +1,21 @@
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
 import PendingIcon from '@mui/icons-material/Pending';
-import { ReactNode } from 'react';
 import { BackgroundCheckStatus } from '@/types/dataModel/volunteer';
 
 const iconSize: string = '30px';
 
 interface BGCIconProps {
-  backgroundCheckStatuses: BackgroundCheckStatus[];
+  backgroundCheckStatus: BackgroundCheckStatus;
 }
 
-export default function BGCIcon({ backgroundCheckStatuses }: BGCIconProps) {
-  return getBGCIcons(backgroundCheckStatuses);
+export default function BGCIcon({ backgroundCheckStatus }: BGCIconProps) {
+  return BGCIcons[backgroundCheckStatus];
 }
 
 // SVG icons for each event Background Check Status
-export const BGCIcons: { [id: string]: ReactNode } = {
-  Passed: (
-    <CheckCircleIcon
-      sx={{ color: 'chartreuse', fontSize: iconSize }}
-      key="Passed Icon"
-    />
-  ),
-  Failed: (
-    <CancelIcon
-      sx={{ color: 'red', fontSize: iconSize }}
-      key="Cancelled Icon"
-    />
-  ),
-  'In Progress': (
-    <PendingIcon
-      sx={{ color: 'gold', fontSize: iconSize }}
-      key="In Progress Icon"
-    />
-  ),
+export const BGCIcons = {
+  Passed: <CheckCircleIcon sx={{ fontSize: iconSize }} color="success" />,
+  Failed: <CancelIcon sx={{ fontSize: iconSize }} color="error" />,
+  'In Progress': <PendingIcon sx={{ fontSize: iconSize }} color="warning" />,
 };
-
-function getBGCIcon(status: string | BackgroundCheckStatus): ReactNode {
-  return BGCIcons[status];
-}
-
-function getBGCIcons(
-  statusList: string[] | BackgroundCheckStatus[]
-): ReactNode[] {
-  return statusList.map((status) => getBGCIcon(status));
-}
