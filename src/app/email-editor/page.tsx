cf068685f45ceb9f5fa111535fb9f4a382a92e85
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-04-05 14:59:28 -0400

    added send email button

--- src/app/email-editor/page.tsx
+++ src/app/email-editor/page.tsx
@@ -1,11 +1,20 @@
-'use client';
 import EmailEditor from '@/components/EmailEditor';
+import { getAllVolunteersForEvent, getEvent } from '@/server/actions/Event';
 
-export default function EmailEditorPage() {
+export default async function EmailEditorPage({
+  params,
+}: {
+  params: { eventId: string };
+}) {
+  const event = await getEvent(params.eventId);
+  const eventVolunteers = await getAllVolunteersForEvent(params.eventId);
+
   return (
     <EmailEditor
       onChange={function (): void {}}
       formData={{ subject: '', emailbody: '' }}
+      event={event}
+      volunteers={eventVolunteers}
     />
   );
 }
