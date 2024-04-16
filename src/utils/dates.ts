234ef5dff040b4ebb15583432c36e881e49d234b
Andrew Rutter <andlrutt@vols.utk.edu>
2024-04-16 02:18:02 -0400

    get minMax date timesamps util func

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,6 +1,16 @@
 import { EventResponse } from '@/types/dataModel/event';
 import { rrulestr } from 'rrule';
 
+export function getMinMaxTimestampsFromYear(year: number) {
+  const minDate = new Date(year, 0, 1);
+  minDate.setUTCHours(0, 0, 0, 0);
+
+  const maxDate = new Date(year, 11, 31);
+  maxDate.setUTCHours(23, 59, 59, 999);
+
+  return [minDate, maxDate];
+}
+
 export function datesBetweenFromRrule(
   rrule: string,
   startDate: Date,
