e1042b53a00c6ed5ccaa60b674934a13e08e9172
JihunKimCode <jiunkim8@gmail.com>
2024-04-19 09:47:49 -0400

    initial push

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,17 @@
+'use client';
+import React, {useState} from 'react';
+// import * as React from 'react';
+import { Tab, Tabs } from '@mui/material';
 import { getEventsBetweenDates } from '@/server/actions/Event';
 import { EventResponse } from '@/types/dataModel/event';
 import EventsView from '@/views/EventsView';
 import dayjs from 'dayjs';
+
+const sortEventsByDateAsc = (events: EventResponse[]) => {
+  return events.sort((a, b) => {
+    return dayjs(a.date).valueOf() - dayjs(b.date).valueOf();
+  });
+};
 
 const sortEventsByDateDesc = (events: EventResponse[]) => {
   return events.sort((a, b) => {
@@ -10,15 +20,47 @@ const sortEventsByDateDesc = (events: EventResponse[]) => {
 };
 
 export default async function EventsPage() {
-  const startOfMonth = dayjs().startOf('month').toDate();
-  const endOfMonth = dayjs().endOf('month').toDate();
-  const events = await getEventsBetweenDates(startOfMonth, endOfMonth);
-  let sortedEvents: EventResponse[];
-  if (!events) {
-    return <div>Failed to load events</div>;
-  } else {
-    sortedEvents = sortEventsByDateDesc(events);
-  }
+  const today = dayjs().startOf('day').toDate();
+  const startOfYear = dayjs().startOf('year').toDate();
+  const endOfYear = dayjs().endOf('year').toDate();
 
-  return <EventsView events={sortedEvents} />;
+  try {
+    const events = await getEventsBetweenDates(startOfYear, endOfYear);
+    
+    if (!events || events.length === 0) {
+      return <div>No events available</div>;
+    }
+
+    let upcomingEvents: EventResponse[];
+    let pastEvents: EventResponse[];
+
+    const eventsBeforeToday = events.filter(event => dayjs(event.date).isBefore(today));
+    const eventsTodayAndAfter = events.filter(event => dayjs(event.date).isSame(today, 'day') || dayjs(event.date).isAfter(today));
+        
+    upcomingEvents = sortEventsByDateAsc(eventsTodayAndAfter);
+    pastEvents = sortEventsByDateDesc(eventsBeforeToday);
+
+    const [tabIndex, setTabIndex] = React.useState(0);
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+      setTabIndex(newValue);
+    };
+
+    return (
+      <>
+        <Tabs value={tabIndex} onChange={handleTabChange} centered>
+          <Tab label="Upcoming Events" />
+          <Tab label="Past Events" />
+        </Tabs>
+        {tabIndex === 0 && (
+          <EventsView events={upcomingEvents} />
+        )}
+        {tabIndex === 1 && (
+          <EventsView events={pastEvents} />
+        )}
+      </>
+    );
+  } catch (error) {
+    console.error('Error fetching events:', error);
+    return <div>Error fetching events: {error.message}. Please try again later.</div>;
+  }
 }
