3c257af26b001205093a9281659355d6a48a1aae
Zavier Miller <zavierjmiller@gmail.com>
2024-04-22 00:54:56 -0400

    callbacks in user auth function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,9 @@
 import { getServerSession } from 'next-auth';
 import CMError, { CMErrorType } from './cmerror';
+import { handler } from '@/app/api/auth/[...nextauth]/auth';
 
 export async function userAuth() {
-  const session = await getServerSession();
+  const session = await getServerSession(handler);
   if (!session) {
     throw new CMError(CMErrorType.Unauthorized);
   }
