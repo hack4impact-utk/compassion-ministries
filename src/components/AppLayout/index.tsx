b1c1460dafdff1bfbf0d6daf1132cc7d2d190497
Andrew Rutter <andlrutt@vols.utk.edu>
2024-04-21 21:40:06 -0400

    AppLayout component with Nav Drawer + Appbar

---
+++ src/components/AppLayout/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Box } from '@mui/material';
+import Providers from '../Providers';
+import React, { useState } from 'react';
+import Appbar from '../Appbar';
+import NavigationDrawer from '../NavigationDrawer';
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+
+  return (
+    <Providers>
+      <Appbar setDrawerOpen={() => setDrawerOpen((open) => !open)} />
+      <Box component="section" sx={{ display: 'flex' }}>
+        <NavigationDrawer
+          open={drawerOpen}
+          closeDrawer={() => setDrawerOpen(false)}
+        />
+        <Box component="main" sx={{ p: 2, flexGrow: 1 }}>
+          {children}
+        </Box>
+      </Box>
+    </Providers>
+  );
+}
