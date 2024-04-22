a44e1fbb5b7dd3571d3c18ebedc799bc32c622b2
Zavier Miller <zavierjmiller@gmail.com>
2024-04-22 00:42:28 -0400

    add guards and cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,11 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: [],
+  matcher: [
+    '/',
+    '/volunteers/:path*',
+    '/events/:path*',
+    '/organizations/:path*',
+    '/settings/:path*',
+  ],
 };
