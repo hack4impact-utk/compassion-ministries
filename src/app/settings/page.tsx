d44915e697dae235b536452659f5a104ed8b7008
Zavier Miller <zavierjmiller@gmail.com>
2024-04-22 01:40:38 -0400

    add auth for settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,14 @@
 import { getAllUsers } from '@/server/actions/User';
+import { adminAuth } from '@/utils/auth';
 import SettingsView from '@/views/SettingsView';
+import { redirect } from 'next/navigation';
 
 export default async function SettingsPage() {
+  try {
+    await adminAuth();
+  } catch (e) {
+    redirect('/');
+  }
   const users = await getAllUsers();
   return <SettingsView users={users} />;
 }
