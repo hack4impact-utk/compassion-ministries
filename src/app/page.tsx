b96335842f246667b4094ecb3dbdafe44d7924cf
Dillon Frankenstein <dfranke2@vols.utk.edu>
2024-04-24 15:07:17 -0400

    Eventspage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Typography } from '@mui/material';
+import EventsPage from './events/page';
 
 export default async function Home() {
-  return <Typography variant="h3">Home Page</Typography>;
+  return <EventsPage />;
 }
