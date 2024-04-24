0a83ea002a14e1943492ecfca9ce51db1349398b
Frank-D-stein <dillonfrankenstein@gmail.com>
2024-04-23 20:42:00 -0400

    conflics handled

--- src/types/forms/events.ts
+++ src/types/forms/events.ts
@@ -7,22 +7,6 @@ export const zTime = z.string().regex(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/, {
   message: 'Invalid time format',
 });
 
-<<<<<<< HEAD
-export const zUpsertEventFormData = z.object({
-  name: z.string().nonempty('Required'),
-  description: z.string().optional(),
-  eventLocation: z.string().optional(),
-  startAt: zDayJs,
-  endAt: zDayJs,
-  date: zDayJs,
-  eventRoles: z.array(zRole, {
-    required_error: 'At least one role is required',
-  }),
-});
-
-export interface UpsertEventFormData
-  extends z.infer<typeof zUpsertEventFormData> {}
-=======
 const zDayJs = z.custom<Dayjs>((val) => val instanceof dayjs, 'Invalid date');
 
 export const zEventFormData = z
@@ -51,5 +35,4 @@ export const zEventFormData = z
     }
   );
 
->>>>>>> 884fecce156fbc2b1e4ab42d7d36c6b2fbc0fab8
 export interface EventFormData extends z.infer<typeof zEventFormData> {}
