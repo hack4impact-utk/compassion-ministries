7d25f35baf5f65408cc5824f24bf78b2fe248911
JihunKimCode <jiunkim8@gmail.com>
2024-04-24 19:15:46 -0400

    tabs done

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,17 +1,7 @@
-'use client';
-import React, {useState} from 'react';
-// import * as React from 'react';
-import { Tab, Tabs } from '@mui/material';
 import { getEventsBetweenDates } from '@/server/actions/Event';
 import { EventResponse } from '@/types/dataModel/event';
 import EventsView from '@/views/EventsView';
 import dayjs from 'dayjs';
-
-const sortEventsByDateAsc = (events: EventResponse[]) => {
-  return events.sort((a, b) => {
-    return dayjs(a.date).valueOf() - dayjs(b.date).valueOf();
-  });
-};
 
 const sortEventsByDateDesc = (events: EventResponse[]) => {
   return events.sort((a, b) => {
@@ -20,47 +10,15 @@ const sortEventsByDateDesc = (events: EventResponse[]) => {
 };
 
 export default async function EventsPage() {
-  const today = dayjs().startOf('day').toDate();
-  const startOfYear = dayjs().startOf('year').toDate();
-  const endOfYear = dayjs().endOf('year').toDate();
-
-  try {
-    const events = await getEventsBetweenDates(startOfYear, endOfYear);
-    
-    if (!events || events.length === 0) {
-      return <div>No events available</div>;
-    }
-
-    let upcomingEvents: EventResponse[];
-    let pastEvents: EventResponse[];
-
-    const eventsBeforeToday = events.filter(event => dayjs(event.date).isBefore(today));
-    const eventsTodayAndAfter = events.filter(event => dayjs(event.date).isSame(today, 'day') || dayjs(event.date).isAfter(today));
-        
-    upcomingEvents = sortEventsByDateAsc(eventsTodayAndAfter);
-    pastEvents = sortEventsByDateDesc(eventsBeforeToday);
-
-    const [tabIndex, setTabIndex] = React.useState(0);
-    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-      setTabIndex(newValue);
-    };
-
-    return (
-      <>
-        <Tabs value={tabIndex} onChange={handleTabChange} centered>
-          <Tab label="Upcoming Events" />
-          <Tab label="Past Events" />
-        </Tabs>
-        {tabIndex === 0 && (
-          <EventsView events={upcomingEvents} />
-        )}
-        {tabIndex === 1 && (
-          <EventsView events={pastEvents} />
-        )}
-      </>
-    );
-  } catch (error) {
-    console.error('Error fetching events:', error);
-    return <div>Error fetching events: {error.message}. Please try again later.</div>;
+  const startOfMonth = dayjs().startOf('year').toDate();
+  const endOfMonth = dayjs().endOf('year').toDate();
+  const events = await getEventsBetweenDates(startOfMonth, endOfMonth);
+  let sortedEvents: EventResponse[];
+  if (!events) {
+    return <div>Failed to load events</div>;
+  } else {
+    sortedEvents = sortEventsByDateDesc(events);
   }
+
+  return <EventsView events={sortedEvents} />;
 }
--- src/views/EventsView/index.tsx
+++ src/views/EventsView/index.tsx
@@ -1,26 +1,55 @@
 'use client';
-import EventList from '@/components/EventList';
+import React, {useState} from 'react';
+import { Box, Button, Link, Tab, Tabs, Typography } from '@mui/material';
 import { EventResponse } from '@/types/dataModel/event';
-import { Box, Button, Typography } from '@mui/material';
-import Link from 'next/link';
+import dayjs from 'dayjs';
+import EventList from '@/components/EventList';
 import SearchField from '@/components/SearchField';
 import useSearch from '@/hooks/useSearch';
 
 interface EventsViewProps {
-  events: EventResponse[];
+  events: EventResponse[]
 }
 
-export default function EventsView({ events }: EventsViewProps) {
-  const search = useSearch();
+const sortEventsByDateAsc = (events: EventResponse[]) => {
+  return events.sort((a, b) => {
+    return dayjs(a.date).valueOf() - dayjs(b.date).valueOf();
+  });
+};
 
-  if (search.length > 0) {
-    events = events.filter((event) =>
-      `${event.name}`.toLowerCase().includes(search.toLowerCase())
-    );
-  }
+const sortEventsByDateDesc = (events: EventResponse[]) => {
+  return events.sort((a, b) => {
+    return dayjs(b.date).valueOf() - dayjs(a.date).valueOf();
+  });
+};
 
-  return (
-    <Box>
+export default function EventsPage({events}: EventsViewProps) {
+    const search = useSearch();
+
+    if (search.length > 0) {
+      events = events.filter((event) =>
+        `${event.name}`.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    let upcomingEvents: EventResponse[];
+    let pastEvents: EventResponse[];
+
+    const today = dayjs().startOf('day').toDate();
+    const eventsBeforeToday = events.filter(event => dayjs(event.date).isBefore(today));
+    const eventsTodayAndAfter = events.filter(event => dayjs(event.date).isSame(today, 'day') || dayjs(event.date).isAfter(today));
+        
+    upcomingEvents = sortEventsByDateAsc(eventsTodayAndAfter);
+    pastEvents = sortEventsByDateDesc(eventsBeforeToday);
+
+    const [tabIndex, setTabIndex] = React.useState(0);
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+      setTabIndex(newValue);
+    };
+
+    return (
+      <>
+      <Box>
       <SearchField />
       <Typography variant="h3" sx={{ my: 2 }}>
         Events
@@ -30,7 +59,17 @@ export default function EventsView({ events }: EventsViewProps) {
           New event
         </Button>
       </Link>
-      <EventList events={events} />
+        <Tabs value={tabIndex} onChange={handleTabChange} centered>
+          <Tab label="Upcoming Events" />
+          <Tab label="Past Events" />
+        </Tabs>
+        {tabIndex === 0 && (
+          <EventList events={upcomingEvents} />
+        )}
+        {tabIndex === 1 && (
+          <EventList events={pastEvents} />
+        )}
     </Box>
-  );
-}
+      </>
+    );
+  }
