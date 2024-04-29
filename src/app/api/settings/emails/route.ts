89ada8afb6e3351152bf4674b6fd9328666a2ffe
ethanhmaness <ethanhmaness@gmail.com>
2024-04-29 16:49:49 -0400

    changed api route to use query param for email

--- src/app/api/settings/emails/route.ts
+++ src/app/api/settings/emails/route.ts
@@ -1,18 +1,16 @@
 import { removeAllowedEmail } from "@/server/actions/Setting";
-import { zRemoveAllowedEmailRequest } from "@/types/dataModel/settings";
-import CMError, { CMErrorResponse, CMErrorType } from "@/utils/cmerror";
+import { CMErrorResponse } from "@/utils/cmerror";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
     request: NextRequest
 ) {
     try {
-        const req = await request.json()
-        const validationResult = zRemoveAllowedEmailRequest.safeParse(req)
-        if (!validationResult.success) {
-            return new CMError(CMErrorType.BadValue, 'Request').toNextResponse();
+        const email = request.nextUrl.searchParams.get("email")
+
+        if (email) {
+            await removeAllowedEmail(email)
         }
-        await removeAllowedEmail(validationResult.data.email)
         return new NextResponse(undefined, { status: 204 })
     }
     catch (e) {
--- src/types/dataModel/settings.ts
+++ src/types/dataModel/settings.ts
@@ -8,12 +8,5 @@ const zSettings = z.object({
 const zSettingsEntity = zSettings.extend(zBase.shape);
 const zSettingsResponse = zSettingsEntity;
 
-const zEmail = z.object({
-  email: z.string()
-});
-
-export const zRemoveAllowedEmailRequest = zEmail;
-
 export interface SettingsEntity extends z.infer<typeof zSettingsEntity> { }
 export interface SettingsResponse extends z.infer<typeof zSettingsResponse> { }
-export interface RemoveAllowedEmailRequest extends z.infer<typeof zRemoveAllowedEmailRequest> { }
