563d271819fcc861a3eaf82fda966b13b8f35e26
Andrew Rutter <andlrutt@vols.utk.edu>
2024-05-01 15:18:59 -0400

    Updated VolunteerList

--- src/components/VolunteerList/index.tsx
+++ src/components/VolunteerList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import VolunteerListItem from '@/components/VolunteerList/VolunteerListItem';
 import { VolunteerResponse } from '@/types/dataModel/volunteer';
 import { List } from '@mui/material';
+import { sortByLastName } from '@/utils/sorting';
 
 interface VolunteerListProps {
   volunteers: VolunteerResponse[];
@@ -11,15 +12,13 @@ export default function VolunteerList({ volunteers }: VolunteerListProps) {
   return (
     <List>
       {/* Sorts volunteerse by last name */}
-      {volunteers
-        .sort((a, b) =>
-          `${a.lastName} ${a.firstName}`.localeCompare(
-            `${b.lastName} ${b.firstName}`
-          )
-        )
-        .map((volunteer) => (
-          <VolunteerListItem key={volunteer._id} volunteer={volunteer} />
-        ))}
+      {sortByLastName<VolunteerResponse>(
+        volunteers,
+        'firstName',
+        'lastName'
+      ).map((volunteer) => (
+        <VolunteerListItem key={volunteer._id} volunteer={volunteer} />
+      ))}
     </List>
   );
 }
