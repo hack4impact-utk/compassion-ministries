5f9413b74e8bde8a347eb985b8df43dbdfa4fd8f
Zavier Miller <zavierjmiller@gmail.com>
2024-05-02 11:55:52 -0400

    connect settings!

--- src/views/SettingsView/UserList/index.tsx
+++ src/views/SettingsView/UserList/index.tsx
@@ -1,24 +1,51 @@
 import { UserResponse } from '@/types/dataModel/user';
 import { sortByPath } from '@/utils/sorting';
+import { Clear } from '@mui/icons-material';
 import {
   Avatar,
+  CircularProgress,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
 } from '@mui/material';
+import { useState } from 'react';
 
 interface UserListProps {
   users: UserResponse[];
-  secondaryAction?: React.ReactNode;
+  onUserDelete: (user: UserResponse) => Promise<void>;
 }
 
-export default function UserList({ users, secondaryAction }: UserListProps) {
+export default function UserList({ users, onUserDelete }: UserListProps) {
+  const [currentlyDeleting, setCurrentlyDeleting] = useState<string | null>(
+    null
+  );
   return (
     <List>
       {/* Sorted by name */}
       {sortByPath<UserResponse>(users, 'name').map((user) => (
-        <ListItem key={user._id} secondaryAction={secondaryAction}>
+        <ListItem
+          key={user._id}
+          secondaryAction={
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              size="small"
+              onClick={async () => {
+                setCurrentlyDeleting(user._id);
+                await onUserDelete(user);
+                setCurrentlyDeleting(null);
+              }}
+            >
+              {currentlyDeleting === user._id ? (
+                <CircularProgress size={24} />
+              ) : (
+                <Clear fontSize="small" />
+              )}
+            </IconButton>
+          }
+        >
           <ListItemAvatar>
             <Avatar src={user.image || ''} />
           </ListItemAvatar>
