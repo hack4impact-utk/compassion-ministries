2b412c1436ac5b4c9938fc4e0ff19d36c3633633
Zavier Miller <zavierjmiller@gmail.com>
2024-05-03 12:50:09 -0400

    remove script

--- scripts/reset-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Resetting database..."
-cat scripts/createTestData.script | mongosh mongodb+srv://hack4impactcm:6VhUxvUZlSc2z7d3@compassionministries.hkxvhf1.mongodb.net/ >/dev/null
-echo "Done."
